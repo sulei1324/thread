9837a0864653f8658afcb1b248a74b7b2212935e
sulei1324 <651343980@qq.com>
2015-06-08 18:23:49 +0800

    ok

---
+++ onethr.py
@@ -0,0 +1,24 @@
+__author__ = 'Su Lei'
+
+from time import sleep, ctime
+
+def loop0():
+    print 'start loop 0 at: ', ctime()
+    sleep(4)
+    print 'loop 0 done at: ', ctime()
+
+def loop1():
+    print 'start loop 1 at: ', ctime()
+    sleep(2)
+    print 'loop 1 done at: ', ctime()
+
+def main():
+    print 'starting at: ', ctime()
+    loop0()
+    loop1()
+    print 'all done at: ', ctime()
+
+if __name__ is '__main__':
+    main()
+
+main()
